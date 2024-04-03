2fa4a8ceb28c9072992b5ba8a197656d871264b6
Pebit <pebitandreiutz@gmail.com>
2024-04-03 14:28:05 +0300

    am modificat obiectul zar

--- wizards.cpp
+++ wizards.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 struct Jucator {
 private:
 	std::string nume;
@@ -11,6 +12,7 @@ public:
 	void set_nume(std::string nume) {
 		this->nume = nume;
 	}
+	// de facut operator de afisare
 };
 
 struct Zar {
@@ -18,10 +20,28 @@ private:
 	int fata[6];
 	bool bust;
 public:
-	Zar() : bust(0), fata {0, 0, 0, 0, 0, 0} {}
+	Zar() : bust(0), fata{ 0, 0, 0, 0, 0, 0 } {}
+	void aruncare() {
+		int r = rand() % 6;
+		std::cout << r;
+		this->fata[r]++;
+		verificare_bust(r);
+	}
+	void verificare_bust(int r) {
+		if (this->fata[r] > 1) {
+			this->bust = 1;
+			std::cout << "\nBUSTT!!!\n";
+		}
+	}
+	bool get_bust() {
+		return this->bust;
+	}
+
 };
 
 int main()
 {
+	srand((int)time(0));
+	Zar zarul_mistic;
 
 }
