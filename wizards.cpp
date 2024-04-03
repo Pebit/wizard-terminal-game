b7fbbcbecfcad6f92eca856a9d40360ff06e980c
Pebit <pebitandreiutz@gmail.com>
2024-04-04 00:01:15 +0300

    modificat afisarile

--- wizards.cpp
+++ wizards.cpp
@@ -9,7 +9,30 @@ public:
 	Zar() : bust(0), fata{ 0, 0, 0, 0, 0, 0 } {}
 	void aruncare() {
 		int r = rand() % 6;
-		std::cout << "a picat " << r;
+		std::cout << "a picat ";
+		switch (r) {
+		case 0:
+			std::cout << "<ENCHANT>";
+			break;
+		case 1:
+			std::cout << "<PROCURA>";
+			break;
+		case 2:
+			std::cout << "<FURA MANA>";
+			break;
+		case 3:
+			std::cout << "<FURA CRISTALE>";
+			break;
+		case 4:
+			std::cout << "<CRISTALIZEAZA/PROCURA>";
+			break;
+		case 5:
+			std::cout << "<CRISTALIZEAZA>";
+			break;
+		default:
+			std::cout << "eroare cod 0";
+			break;
+		}
 		this->fata[r]++;
 		verificare_bust(r);
 	}
@@ -47,9 +70,9 @@ private:
 	int mana;
 	int cristale;
 public:
-	Jucator() : nume("Jucator_Default"), mana(0), cristale(0) {}
-	Jucator(int index) : nume("Jucator_Default"), mana(0), cristale(0) {}
-	Jucator(int index, std::string nume) : nume(nume), mana(0), cristale(0) {}
+	Jucator() : nume("Jucator_Default"), mana(200), cristale(0) {}
+	Jucator(int index) : nume("Jucator_Default"), mana(200), cristale(0) {}
+	Jucator(int index, std::string nume) : nume(nume), mana(200), cristale(0) {}
 
 	std::string get_nume();
 	void procura() {
@@ -59,9 +82,11 @@ public:
 		if (this->mana > 100) {
 			this->cristale += 100;
 			this->mana -= 100;
+			std::cout << "ai CRISTALIZAT 100 ";
 		}
 		else {
 			this->cristale += mana;
+			std::cout << "ai CRISTALIZAT" << mana << " ";
 			mana = 0;
 		}
 	}
@@ -87,24 +112,27 @@ public:
 		if (other.mana > 50) {
 			other.mana -= 50;
 			this->mana += 50;
+			std::cout << "ai FURAT 50 ";
 		}
 		else {
-			other.mana = 0;
 			this->mana += other.mana;
+			std::cout << "ai FURAT " << other.mana << " ";
+			other.mana = 0;
 		}
 	}
 	void fura_cristale(Jucator& other) {
 		if (other.cristale > 35) {
 			other.cristale -= 35;
 			this->mana += 35;
+			std::cout << "ai FURAT 35 ";
 		}
 		else {
-			other.cristale = 0;
 			this->mana += other.cristale;
+			std::cout << "ai FURAT " << other.cristale << " ";
+			other.cristale = 0;
 		}
 	}
 	void enchant(Zar& zar) {
-		std::cout << "puterea de amplificat va fi:\n 1:procura(full)\n2:fura mana\n3:fura cristale\n4:cristalizeaza/procura\n5:cristalizeaza(full)\n";
 		int decider = -1;
 		while (decider < 1 or decider > 5)
 			std::cin >> decider;
@@ -125,7 +153,7 @@ public:
 			zar.inc_fata(5);
 			break;
 		default:
-			zar.inc_fata(1);
+			std::cout << "eroare cod 2";
 			break;
 		}
 	}
@@ -143,7 +171,7 @@ std::istream& operator>>(std::istream& is, Jucator& jucator) {
 	return is;
 }
 std::ostream& operator<<(std::ostream& os, const Jucator& jucator) {
-	os << "Nume:" << jucator.nume << ": " << jucator.mana << "m " << jucator.cristale << "c\n";
+	os << jucator.nume << " - " << jucator.mana << "m " << jucator.cristale << "c\n";
 	return os;
 }
 
@@ -154,9 +182,12 @@ int main()
 	Zar zarul_mistic;
 	int nr_runde = 2;
 
-	int nr_jucatori;
-	std::cout << "introduceti numarul de jucatori (maxim 7)\n";
-	std::cin >> nr_jucatori;
+	int nr_jucatori = -1;
+	while (nr_jucatori < 2 or nr_jucatori > 7)
+	{
+		std::cout << "introduceti numarul de jucatori (maxim 7)\n";
+		std::cin >> nr_jucatori;
+	}
 
 	Jucator arrJucatori[8];
 	for (int i = 1; i <= nr_jucatori; i++)
@@ -184,8 +215,33 @@ int main()
 				std::cin >> maiarunci;
 			}
 			if (not zarul_mistic.get_bust()) {
-				if (zarul_mistic.get_fata(0))
+				if (zarul_mistic.get_fata(0)) {
+					for (int fata = 1; fata < 6; fata++)
+						if (zarul_mistic.get_fata(fata)) {
+							switch (fata) {
+							case 1:
+								std::cout << "1 <PROCURA>\n";
+								break;
+							case 2:
+								std::cout << "2 <FURA MANA>\n";
+								break;
+							case 3:
+								std::cout << "3 <FURA CRISTALE>\n";
+								break;
+							case 4:
+								std::cout << "4 <CRISTALIZEAZA/PROCURA>\n";
+								break;
+							case 5:
+								std::cout << "5 <CRISTALIZEAZA>\n";
+								break;
+							default:
+								std::cout << "eroare cod 3\n";
+								break;
+							}
+						}
+
 					arrJucatori[id_jucator].enchant(zarul_mistic);
+				}
 				for (int fata = 1; fata < 6; fata++) {
 					while (zarul_mistic.get_fata(fata)) {
 						int jucator_ales = -1;
@@ -195,27 +251,31 @@ int main()
 							std::cout << "ai PROCURAT 100 mana\n";
 							break;
 						case 2:
-							std::cout << "alege un jucator de la care sa furi (" << 1 << "->" << nr_jucatori << ")\n";
-							while ((jucator_ales < 1 or jucator_ales > nr_jucatori) and jucator_ales != id_jucator)
+							while ((jucator_ales < 1 or jucator_ales > nr_jucatori) and jucator_ales != id_jucator) {
+								std::cout << "alege un jucator de la care sa furi (" << 1 << "->" << nr_jucatori << ")\n";
 								std::cin >> jucator_ales;
+							}
 							arrJucatori[id_jucator].fura_mana(arrJucatori[jucator_ales]);
 							std::cout << "ai FURAT 50 mana de la\n" << arrJucatori[jucator_ales] << "\n";
 							break;
 						case 3:
-							std::cout << "alege un jucator de la care sa furi (" << 1 << "->" << nr_jucatori << ")\n";
-							while ((jucator_ales < 1 or jucator_ales > nr_jucatori) and jucator_ales != id_jucator)
+							while ((jucator_ales < 1 or jucator_ales > nr_jucatori) and jucator_ales != id_jucator) {
+								std::cout << "alege un jucator de la care sa furi (" << 1 << "->" << nr_jucatori << ")\n";
 								std::cin >> jucator_ales;
+							}
 							arrJucatori[id_jucator].fura_cristale(arrJucatori[jucator_ales]);
-							std::cout << "ai FURAT 35 cristale de la\n" << arrJucatori[jucator_ales] << " si s-au transformat in mana\n";
+							std::cout << "cristale de la\n" << arrJucatori[jucator_ales] << " si s - au transformat in mana\n" << arrJucatori[id_jucator] << "\n";
 							break;
 						case 4:
 							arrJucatori[id_jucator].crist_or_proc();
+							std::cout << arrJucatori[id_jucator] << "\n";
 							break;
 						case 5:
 							arrJucatori[id_jucator].cristalizeaza();
+							std::cout << "din mana\n" << arrJucatori[id_jucator] << "\n";
 							break;
 						default:
-							std::cout << "eroare citire fete";
+							std::cout << "eroare bug in cod 1";
 							break;
 						}
 						zarul_mistic.dec_fata(fata);
