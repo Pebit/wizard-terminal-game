b8ecd4246b61bb456b8ea20d6e61173a5a0e88f5
Pebit <pebitandreiutz@gmail.com>
2024-04-05 00:14:34 +0300

    curatat afisarile si obiectelel

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # wizard-terminal-game
+
+
+
+Jocul are 2 runde. Daca la finalul jocului ai cele mai multe cristale (puncte), ai castigat.
+
+Un jucator are doar 2 atribute: mana si cristale
+mana - punctele inainte de a fi transformate in cristale
+cristale - punctele finale care vor fi numarate la finalul jocului
+
+Jucatorii incep cu 200 mana si 0 cristale
+
+Intr-o runda un jucator poate sa dea cu zarul de cate ori doreste. Daca acesta da acelasi zar de 2 ori da "BUSTT!!!" si pierde tot ce i-a picat. Daca acesta se opreste inainte de a da "BUSTT!!!" executa in ordine predefinita (cea mai fireasca) fetele de zar picate.
+
+Acestea sunt fetele si rolul fiecareia:
+    -procura: va adauga un 100 de mana in mana jucatorului
+    -cristalizeaza: va aduce 100 de mana la forma de cristale(puncte).
+    -cristalizeaza/procura: va cristaliza sau procura la alegere 50 de mana.
+    -fura mana: lasa jucatorul sa aleaga alt player si apoi va fura 50 mana de la acesta
+    -fura cristale: lasa jucatorul sa aleaga alt player si apoi va fura 35 cristale de la acesta si le v-a primii sub forma de mana
+    -enchanteaza: dubleaza o actiune la alegere
--- wizards.cpp
+++ wizards.cpp
@@ -7,62 +7,61 @@ private:
 	bool bust;
 public:
 	Zar() : bust(0), fata{ 0, 0, 0, 0, 0, 0 } {}
-	void aruncare() {
+
+	bool get_bust() { return this->bust; }
+	int get_fata(int i) { return this->fata[i]; }
+	void inc_fata(int i) { this->fata[i]++; }
+	void dec_fata(int i) { this->fata[i]--; }
+
+	void aruncare();
+	void verificare_bust(int r);
+	void memory_wipe();
+};
+
+void Zar::memory_wipe() {
+	this->bust = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		fata[i] = 0;
+	}
+}
+void Zar::aruncare() {
+	{
 		int r = rand() % 6;
 		std::cout << "a picat ";
 		switch (r) {
 		case 0:
-			std::cout << "<ENCHANT>";
+			std::cout << "<ENCHANT>\n";
 			break;
 		case 1:
-			std::cout << "<PROCURA>";
+			std::cout << "<PROCURA>\n";
 			break;
 		case 2:
-			std::cout << "<FURA MANA>";
+			std::cout << "<FURA MANA>\n";
 			break;
 		case 3:
-			std::cout << "<FURA CRISTALE>";
+			std::cout << "<FURA CRISTALE>\n";
 			break;
 		case 4:
-			std::cout << "<CRISTALIZEAZA/PROCURA>";
+			std::cout << "<CRISTALIZEAZA/PROCURA>\n";
 			break;
 		case 5:
-			std::cout << "<CRISTALIZEAZA>";
+			std::cout << "<CRISTALIZEAZA>\n";
 			break;
 		default:
-			std::cout << "eroare cod 0";
+			std::cout << "eroare cod 0\n";
 			break;
 		}
 		this->fata[r]++;
 		verificare_bust(r);
 	}
-	void verificare_bust(int r) {
-		if (this->fata[r] > 1) {
-			this->bust = 1;
-			std::cout << "\nBUSTT!!!\n";
-		}
+}
+void Zar::verificare_bust(int r) {
+	if (this->fata[r] > 1) {
+		this->bust = 1;
+		std::cout << "BUSTT!!!\n";
 	}
-	bool get_bust() {
-		return this->bust;
-	}
-	void inc_fata(int i) {
-		this->fata[i]++;
-	}
-	void dec_fata(int i) {
-		this->fata[i]--;
-	}
-	int get_fata(int i) {
-		return this->fata[i];
-	}
-	void memory_wipe()
-	{
-		this->bust = 0;
-		for (int i = 0; i < 6; i++)
-		{
-			fata[i] = 0;
-		}
-	}
-};
+}
 
 struct Jucator {
 private:
@@ -76,91 +75,13 @@ public:
 
 	std::string get_nume();
 	int get_cristale();
-	void procura() {
-		this->mana += 100;
-	}
-	void cristalizeaza() {
-		if (this->mana > 100) {
-			this->cristale += 100;
-			this->mana -= 100;
-			std::cout << "ai CRISTALIZAT 100 ";
-		}
-		else {
-			this->cristale += mana;
-			std::cout << "ai CRISTALIZAT" << mana << " ";
-			mana = 0;
-		}
-	}
-	void crist_or_proc() {
-		std::cout << "cristalizeaza(0), procura(1)";
-		bool decider;
-		std::cin >> decider;
-		if (decider) {
-			this->mana += 50;
-		}
-		else {
-			if (this->mana > 50) {
-				this->cristale += 50;
-				this->mana -= 50;
-			}
-			else {
-				this->cristale += mana;
-				mana = 0;
-			}
-		}
-	}
-	void fura_mana(Jucator& other) {
-		if (other.mana > 50) {
-			other.mana -= 50;
-			this->mana += 50;
-			std::cout << "ai FURAT 50 ";
-		}
-		else {
-			this->mana += other.mana;
-			std::cout << "ai FURAT " << other.mana << " ";
-			other.mana = 0;
-		}
-	}
-	void fura_cristale(Jucator& other) {
-		if (other.cristale > 35) {
-			other.cristale -= 35;
-			this->mana += 35;
-			std::cout << "ai FURAT 35 ";
-		}
-		else {
-			this->mana += other.cristale;
-			std::cout << "ai FURAT " << other.cristale << " ";
-			other.cristale = 0;
-		}
-	}
-	void enchant(Zar& zar) {
-		int decider = -1;
-		while (decider < 1 or decider > 5)
-			std::cin >> decider;
-		switch (decider) {
-		case 1:
-			zar.inc_fata(1);
-			break;
-		case 2:
-			zar.inc_fata(2);
-			break;
-		case 3:
-			zar.inc_fata(3);
-			break;
-		case 4:
-			zar.inc_fata(4);
-			break;
-		case 5:
-			zar.inc_fata(5);
-			break;
-		default:
-			std::cout << "eroare cod 2";
-			break;
-		}
-	}
-	void consolare() {
-		this->mana += 50;
-	}
+	void procura();
+	void cristalizeaza();
+	void crist_or_proc();
+	void fura_mana(Jucator& other);
+	void fura_cristale(Jucator& other);
+	void enchant(Zar& zar);
+	void consolare();
 
 	friend std::istream& operator>>(std::istream& in, Jucator& jucator);
 	friend std::ostream& operator<<(std::ostream& os, const Jucator& jucator);
@@ -172,6 +93,93 @@ std::string Jucator::get_nume() {
 int Jucator::get_cristale() {
 	return this->cristale;
 }
+
+void Jucator::procura() {
+	this->mana += 100;
+}
+void Jucator::cristalizeaza() {
+	if (this->mana > 100) {
+		this->cristale += 100;
+		this->mana -= 100;
+		std::cout << "ai CRISTALIZAT 100 ";
+	}
+	else {
+		this->cristale += mana;
+		std::cout << "ai CRISTALIZAT" << mana << " ";
+		mana = 0;
+	}
+}
+void Jucator::crist_or_proc() {
+	std::cout << "cristalizeaza(0), procura(1)";
+	bool decider;
+	std::cin >> decider;
+	if (decider) {
+		this->mana += 50;
+	}
+	else {
+		if (this->mana > 50) {
+			this->cristale += 50;
+			this->mana -= 50;
+		}
+		else {
+			this->cristale += mana;
+			mana = 0;
+		}
+	}
+}
+void Jucator::fura_mana(Jucator& other) {
+	if (other.mana > 50) {
+		other.mana -= 50;
+		this->mana += 50;
+		std::cout << "ai FURAT 50 ";
+	}
+	else {
+		this->mana += other.mana;
+		std::cout << "ai FURAT " << other.mana << " ";
+		other.mana = 0;
+	}
+}
+void Jucator::fura_cristale(Jucator& other) {
+	if (other.cristale > 35) {
+		other.cristale -= 35;
+		this->mana += 35;
+		std::cout << "ai FURAT 35 ";
+	}
+	else {
+		this->mana += other.cristale;
+		std::cout << "ai FURAT " << other.cristale << " ";
+		other.cristale = 0;
+	}
+}
+void Jucator::enchant(Zar& zar) {
+	int decider = -1;
+	while (decider < 1 or decider > 5)
+		std::cin >> decider;
+	switch (decider) {
+	case 1:
+		zar.inc_fata(1);
+		break;
+	case 2:
+		zar.inc_fata(2);
+		break;
+	case 3:
+		zar.inc_fata(3);
+		break;
+	case 4:
+		zar.inc_fata(4);
+		break;
+	case 5:
+		zar.inc_fata(5);
+		break;
+	default:
+		std::cout << "eroare cod 2";
+		break;
+	}
+}
+void Jucator::consolare() {
+	this->mana += 50;
+}
+
 std::istream& operator>>(std::istream& is, Jucator& jucator) {
 	is >> jucator.nume;
 	return is;
@@ -181,26 +189,46 @@ std::ostream& operator<<(std::ostream& os, const Jucator& jucator) {
 	return os;
 }
 
-int main()
-{
-	srand((int)time(0));
 
-	Zar zarul_mistic;
-	int nr_runde = 2;
-
-	int nr_jucatori = -1;
+void citire_jucatori(int& nr_jucatori, Jucator arrJucatori[]) {
+	nr_jucatori = -1;
 	while (nr_jucatori < 2 or nr_jucatori > 7)
 	{
 		std::cout << "introduceti numarul de jucatori (maxim 7)\n";
 		std::cin >> nr_jucatori;
 	}
-
-	Jucator arrJucatori[8];
 	for (int i = 1; i <= nr_jucatori; i++)
 	{
 		std::cout << "vrajitor " << i << ":";
 		std::cin >> arrJucatori[i];
 	}
+}
+int max_score(int nr_jucatori, Jucator arrJucatori[]) {
+	int max = -1;
+	for (int id_jucator = 1; id_jucator <= nr_jucatori; id_jucator++) {
+		std::cout << id_jucator << " " << arrJucatori[id_jucator];
+		if (arrJucatori[id_jucator].get_cristale() > max)
+			max = arrJucatori[id_jucator].get_cristale();
+	}
+	return max;
+}
+void afisare_castigatori(int nr_jucatori, Jucator arrJucatori[], int max) {
+	// inca un for in caz de egalitate
+	for (int id_jucator = 1; id_jucator <= nr_jucatori; id_jucator++) {
+		if (arrJucatori[id_jucator].get_cristale() == max)
+			std::cout << "Castigator: " << arrJucatori[id_jucator];
+	}
+}
+
+int main()
+{
+	srand((int)time(0));
+
+	Zar zarul_mistic;
+	int nr_runde = 4;
+	int nr_jucatori;
+	Jucator arrJucatori[8];
+	citire_jucatori(nr_jucatori, arrJucatori);
 
 	for (int runda = 1; runda <= nr_runde; runda++) {
 		std::cout << "RUNDA" << runda << '\n';
@@ -209,7 +237,7 @@ int main()
 			std::cout << arrJucatori[id_jucator];
 			zarul_mistic.memory_wipe();
 
-			int maiarunci = 1;
+			bool maiarunci = 1;
 			while (maiarunci) {
 				zarul_mistic.aruncare();
 
@@ -217,7 +245,7 @@ int main()
 					break;
 				}
 
-				std::cout << "\nMai dai cu zarul, " << arrJucatori[id_jucator].get_nume() << " ?\n 0 = nu sau 1 = da:";
+				std::cout << "Mai dai cu zarul, " << arrJucatori[id_jucator].get_nume() << " ?\n 0 = nu sau 1 = da: ";
 				std::cin >> maiarunci;
 			}
 			if (not zarul_mistic.get_bust()) {
@@ -262,7 +290,7 @@ int main()
 								std::cin >> jucator_ales;
 							}
 							arrJucatori[id_jucator].fura_mana(arrJucatori[jucator_ales]);
-							std::cout << "ai FURAT 50 mana de la\n" << arrJucatori[jucator_ales] << "\n";
+							std::cout << "mana de la\n" << arrJucatori[jucator_ales] << "\n";
 							break;
 						case 3:
 							while ((jucator_ales < 1 or jucator_ales > nr_jucatori) and jucator_ales != id_jucator) {
@@ -290,19 +318,10 @@ int main()
 			}
 			else {
 				arrJucatori[id_jucator].consolare();
-				std::cout << "consolare +50\n" << arrJucatori[id_jucator];
+				std::cout << "consolare +50\n" << arrJucatori[id_jucator] << "\n";
 			}
 		}
 	}
-	int max = -1;
-	for (int id_jucator = 1; id_jucator <= nr_jucatori; id_jucator++) {
-		std::cout << id_jucator << " " << arrJucatori[id_jucator];
-		if (arrJucatori[id_jucator].get_cristale() > max)
-			max = arrJucatori[id_jucator].get_cristale();
-	}
-	// inca un for in caz de egalitate
-	for (int id_jucator = 1; id_jucator <= nr_jucatori; id_jucator++) {
-		if (arrJucatori[id_jucator].get_cristale() == max)
-			std::cout << "Castigator: " << arrJucatori[id_jucator];
-	}
+	int max = max_score(nr_jucatori, arrJucatori);
+	afisare_castigatori(nr_jucatori, arrJucatori, max);
 }
